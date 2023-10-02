91b1375107f7ff5715488abd48b1fefc69afaccb
DaRaForU <san.sovandara.fe@gmail.com>
2023-10-02 12:11:08 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let count = 10;
+
+const countHeaDer = document.getElementById('count');
+const countAddBtn = document.getElementById('add');
+const countSubtractBtn = document.getElementById('subtract');
+
+
+countHeaDer.innerHTML = count;
+
+countAddBtn.addEventListener('click', () => {
+    count += 1;
+    countHeaDer.innerHTML = count;
+});
+
+countSubtractBtn.addEventListener('click', () => {
+    count -= 1;
+    countHeaDer.innerHTML = count;
+});
